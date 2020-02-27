c13709ae2310d3763f951ac1ff6d5c5a839d6b07
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-27 16:40:23 -0500

    Implement form validation for name, email, password, and terms using Yup

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,13 +1,39 @@
 import React from "react";
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form, Field, yupToFormErrors } from "formik";
+import * as Yup from "yup";
 
-const UserForm = () => {
+const UserForm = ({values, handleChange, touched, errors, status}) => {
     return (
         <Form>
+            <label htmlFor = "name"> Name
             <Field id = "name" type = "text" name = "name" placeholder = "Name" />
+            {touched.name && errors.name && (
+                <p>{errors.name}</p>
+            )}
+            </label>
+            <br/>
+            <label htmlFor = "email"> Email
             <Field id = "email" type = "email" name = "email" placeholder = "Email" />
+            {touched.email && errors.email && (
+                <p>{errors.email}</p>
+            )}
+            </label>
+            <br/>
+            <label htmlFor = "password"> Password
             <Field id = "password" type = "password" name = "password" placeholder = "Password" />
+            {touched.password && errors.password && (
+                <p>{errors.password}</p>
+            )}
+            </label>
+            <br/>
+            <label htmlFor = "terms"> Agree to Terms of Services
             <Field id = "terms" type = "checkbox" name = "terms"/>
+            {touched.terms && errors.terms && (
+                <p>{errors.terms}</p>
+            )}
+            </label>
+            <br/>
+            <button type = "submit">Submit</button>
         </Form>
     );
 };
@@ -20,7 +46,13 @@ const FormikUserForm = withFormik({
             password: password || "",
             terms: terms || false
         };
-    }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Name is required."),
+        email: Yup.string().required("Email is required."),
+        password: Yup.string().required("Password is required.").min(8, "Password must be at least 8 characters."),
+        terms: Yup.boolean().oneOf([true], "You must agree to the Terms of Services.")
+    })
 })(UserForm);
 
 export default FormikUserForm;
