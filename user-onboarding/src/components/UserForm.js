abf21475db3e1a6c2abc79adbc5ed2e0d93ecd71
Zahid Khawaja <zahidkhawaja@icloud.com>
2020-02-27 18:37:42 -0500

    Remove unnecessary Yup import

--- user-onboarding/src/components/UserForm.js
+++ user-onboarding/src/components/UserForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { withFormik, Form, Field, yupToFormErrors } from "formik";
+import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
